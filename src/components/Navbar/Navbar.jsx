2a59804ae9623bbf42cc9fd02a43f157e4ed585f
Muhammad Shayan <shayanarif666@gmail.com>
2024-11-18 21:04:58 +0500

    Changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,16 +4,17 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import { FaRegUser } from "react-icons/fa";
 import { LuShoppingCart } from "react-icons/lu";
 import { Link } from 'react-router-dom';
+import "./navbar.css";
 
 function Navbar() {
     return (
         <nav className='navbar bg-red-600 py-4' id='navbar' style={{ maxHeight: "13vh" }}>
             <div className="container-fluid d-block">
                 <div className="row flex items-center" style={{ marginTop: "-2.3rem" }}>
-                    <div className="col-lg-2 col-xl-2">
+                    <div className="logo-area col-lg-2 col-xl-2 col-4 order-1">
                         <Logo />
                     </div>
-                    <div className="col-lg-5 col-xl-6 m-auto">
+                    <div className="search-bar col-lg-5 col-xl-6 m-auto order-lg-2 order-3">
                         <Input
                             type={"search"}
                             placeholder='Search everything at Wallmart online store'
@@ -21,7 +22,7 @@ function Navbar() {
                             style={{ borderRadius: "25px", border: "none", padding: ".8rem 1.2rem" }}
                         />
                     </div>
-                    <div className="col-lg-5 col-xl-4 ms-auto border-green-700">
+                    <div className="menu-items col-lg-5 col-xl-4 ms-auto border-green-700 col-8 order-lg-3 order-2">
                         <div className="d-flex align-items-center justify-content-end">
                             <Link to={`/wishlist/${2}`} className="d-flex align-items-center hover:bg-red-700 hover:cursor-pointer py-1 px-2 transition-all duration-200 ease-in-out ms-3">
                                 <IoMdHeartEmpty className='text-3xl text-white font-bold' />
